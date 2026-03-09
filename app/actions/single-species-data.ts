85b2b8f8f37423bbec19b2721d4e379e70517178
Rhys Evans <rhys.evans@ft.com>
2026-03-09 21:44:01 +0000

    Switch to ordinary select query for paginated birds

--- app/actions/single-species-data.ts
+++ app/actions/single-species-data.ts
@@ -1,64 +1,53 @@
 import { SPECIES_PAGE_BATCH_SIZE } from '@/app/constants';
 import {
-	orderBirdsByRecency,
 	enrichBird,
-	type EncounterOfBird,
 	type BirdOfSpecies,
 	type EnrichedBirdOfSpecies
 } from '@/app/models/bird';
-import type { PaginatedBirdsResult } from '@/app/models/db';
 import { getAuthenticatedSupabaseClient } from '@/lib/group-auth';
 import { catchSupabaseErrors } from '@/lib/supabase';
 
-function convertPaginatedBirdResultsToBirds(
-	paginatedBirdResults: PaginatedBirdsResult[]
-): BirdOfSpecies[] {
-	const birdsMap: Record<string, BirdOfSpecies> = {};
-	paginatedBirdResults.forEach((result) => {
-		if (!birdsMap[result.ring_no]) {
-			birdsMap[result.ring_no] = {
-				id: result.bird_id,
-				ring_no: result.ring_no,
-				encounters: [] as EncounterOfBird[]
-			} as BirdOfSpecies;
-		}
-		birdsMap[result.ring_no].encounters.push({
-			id: result.encounter_id,
-			capture_time: result.capture_time,
-			age_code: result.age_code,
-			is_juv: result.is_juv,
-			minimum_years: result.minimum_years,
-			// min_hatch_year: result.min_hatch_year,
-			// max_hatch_year: result.max_hatch_year,
-			record_type: result.record_type,
-			sex: result.sex,
-			weight: result.weight,
-			wing_length: result.wing_length,
-			session: {
-				id: result.session_id,
-				visit_date: result.visit_date
-			}
-		} as EncounterOfBird);
-	});
-	return Object.values(birdsMap);
-}
-
 export async function fetchPageOfBirds(species: string, page: number = 0) {
 	const supabase = await getAuthenticatedSupabaseClient();
+	const { id: speciesId } = (await supabase
+		.from('Species')
+		.select('id')
+		.eq('species_name', species)
+		.single()
+		.then(catchSupabaseErrors)) as { id: number };
+	if (!speciesId) {
+		throw new Error(`Species ${species} not found`);
+	}
 	const paginatedBirdResults = (await supabase
-		.rpc('paginated_birds_table', {
-			species_name_param: species,
-			result_limit: SPECIES_PAGE_BATCH_SIZE,
-			result_offset: page * SPECIES_PAGE_BATCH_SIZE
-		})
-		.then(catchSupabaseErrors)) as PaginatedBirdsResult[];
-
-	return orderBirdsByRecency<EnrichedBirdOfSpecies>(
-		convertPaginatedBirdResultsToBirds(paginatedBirdResults).map(enrichBird),
-		{
-			direction: 'desc',
-			type: 'last',
-			encountersAlreadySorted: true
-		}
-	);
+		.from('Birds')
+		.select(
+			`
+			id,
+			ring_no,
+			last_encountered_timestamp,
+			encounters:Encounters (
+				id,
+				capture_time,
+				min_hatch_year,
+				max_hatch_year,
+				age_code,
+				is_juv,
+				record_type,
+				sex,
+				weight,
+				wing_length,
+				session:Sessions (
+					id,
+					visit_date
+				)
+			)`
+		)
+		.eq('species_id', speciesId)
+		.order('last_encountered_timestamp', { ascending: false })
+		.range(
+			page * SPECIES_PAGE_BATCH_SIZE,
+			(page + 1) * SPECIES_PAGE_BATCH_SIZE - 1
+		)
+		.then(catchSupabaseErrors)) as BirdOfSpecies[];
+	return paginatedBirdResults.map(enrichBird) as EnrichedBirdOfSpecies[];
 }
