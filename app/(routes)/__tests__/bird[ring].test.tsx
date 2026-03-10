8f82f7d8c0c1864cfafe2ce674d3e43bae783e4e
Rhys Evans <rhys.evans@ft.com>
2026-03-10 09:33:51 +0000

    use more representative test data

--- app/(routes)/__tests__/bird[ring].test.tsx
+++ app/(routes)/__tests__/bird[ring].test.tsx
@@ -7,41 +7,44 @@ describe('bird page', () => {
 	it('should show correct heading', async () => {
 		render(
 			await Page({
-				params: new Promise((resolve) => resolve({ ring: 'BVB4353' }))
+				params: new Promise((resolve) => resolve({ ring: 'BVB4138' }))
 			})
 		);
 		const heading = await screen.findByRole('heading', {
 			level: 1
 		});
-		expect(heading.textContent).toBe(" Cetti's Warbler  BVB4353");
+		expect(heading.textContent).toBe(" Cetti's Warbler  BVB4138");
 	});
 	it('should show headline stats', async () => {
 		render(
 			await Page({
-				params: new Promise((resolve) => resolve({ ring: 'BVB4353' }))
+				params: new Promise((resolve) => resolve({ ring: 'BVB4138' }))
 			})
 		);
 		const headlineStats = await screen.findByTestId('bird-stats');
 		const statsLineItems = getAllByRole(headlineStats, 'listitem');
 		expect(statsLineItems).toHaveLength(5);
-		expect(statsLineItems[0].textContent).toBe('3 encounters');
-		expect(statsLineItems[1].textContent).toBe('First: 09 November 2025');
-		expect(statsLineItems[2].textContent).toBe('Last: 14 January 2026');
+		expect(statsLineItems[0].textContent).toBe('6 encounters');
+		expect(statsLineItems[1].textContent).toBe('First: 05 October 2024');
+		expect(statsLineItems[2].textContent).toBe('Last: 05 March 2026');
 		expect(statsLineItems[3].textContent).toBe('Sex: F');
-		expect(statsLineItems[4].textContent).toBe('Proven Age: 1');
+		expect(statsLineItems[4].textContent).toBe('Proven Age: 2');
 	});
 	it('should show table of every encounter', async () => {
 		render(
 			await Page({
-				params: new Promise((resolve) => resolve({ ring: 'BVB4353' }))
+				params: new Promise((resolve) => resolve({ ring: 'BVB4138' }))
 			})
 		);
 		const encounterTable = await screen.findByTestId('single-bird-table');
 		verifyTableData(encounterTable, [
 			['Date', 'Time', 'Age', 'Sex', 'Wing', 'Weight'],
-			['09 Nov 2025', '07:20:00', '3', 'U', '55', '11.3'],
-			['20 Dec 2025', '08:35:00', '2', 'F', '55', '11.7'],
-			['14 Jan 2026', '08:00:00', '4', 'F', '56', '11.1']
+			['05 Oct 2024', '07:00:00', '3', 'U', '', ''],
+			['12 Oct 2024', '07:30:00', '3', 'U', '54', '11.3'],
+			['17 Jan 2025', '08:00:00', '4', 'U', '54', '10.2'],
+			['14 Sep 2025', '07:15:00', '4', 'F', '57', '11.5'],
+			['12 Oct 2025', '08:30:00', '2', 'F', '55', '11.6'],
+			['05 Mar 2026', '09:00:00', '4', 'F', '57', '11.8']
 		]);
 	});
 });
