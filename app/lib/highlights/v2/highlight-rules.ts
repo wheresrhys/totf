ad2909bc95bb778bcd381edddc08d4f781a233be
Rhys Evans <rhys.evans@ft.com>
2026-09-20 22:29:24 +0100

    Rules are defined in a separate file to the engine that runs them

---
+++ app/lib/highlights/v2/highlight-rules.ts
@@ -0,0 +1,94 @@
+import type { Highlight, HighlightsOfType, HighlightTemporalUnit } from './';
+import type { CoreStatsResult } from '@/app/models/db';
+
+export const DEFAULT_OPTIONS = { limit: 3, threshold: 0 };
+
+type HighlightsGenerator = Omit<
+	HighlightsOfType,
+	'highlights' | 'temporalUnit'
+> & {
+	generator: (stats: CoreStatsResult[]) => Highlight[];
+	condition?: (temporalUnit: HighlightTemporalUnit) => boolean;
+};
+interface TimePeriodedItem {
+	time_period: string | null;
+}
+
+type HighlightFinderOptions = {
+	threshold?: number;
+};
+
+function sumProperties<T>(item: T, properties: (keyof T)[]) {
+	return properties.reduce(
+		(sum, property) => sum + ((item[property] as number) ?? 0),
+		0
+	);
+}
+
+function getTopByPropertiesSum<T extends TimePeriodedItem>(
+	properties: (keyof T)[],
+	options?: HighlightFinderOptions
+): (stats: T[]) => Highlight[] {
+	const { threshold } = {
+		...DEFAULT_OPTIONS,
+		...(options || {})
+	};
+	return (rawStats: T[]) =>
+		rawStats
+			.map((item) => ({
+				time_period: item.time_period as string,
+				value: sumProperties(item, properties)
+			}))
+			.filter((item) => item.value > threshold)
+			.sort((a, b) => b.value - a.value);
+}
+
+function getTopByProperty<T extends TimePeriodedItem>(
+	property: keyof T,
+	options?: HighlightFinderOptions
+): (stats: T[]) => Highlight[] {
+	return getTopByPropertiesSum([property], options);
+}
+
+export const highlightRules: HighlightsGenerator[] = [
+	{
+		type: 'birds',
+		unit: 'bird',
+		verb: 'Busiest',
+		category: 'count',
+		generator: getTopByProperty<CoreStatsResult>('bird_count')
+	},
+	{
+		type: 'encounters',
+		unit: 'encounter',
+		verb: 'Most encounters per',
+		category: 'count',
+		generator: getTopByProperty<CoreStatsResult>('encounter_count'),
+		condition: (temporalUnit: HighlightTemporalUnit) => temporalUnit !== 'day'
+	},
+	{
+		type: 'species',
+		unit: 'species',
+		verb: 'Most varied',
+		category: 'count',
+		generator: getTopByProperty<CoreStatsResult>('species_count')
+	},
+	{
+		type: 'newBirds',
+		category: 'count',
+		unit: 'bird',
+		verb: 'Most new birds in a',
+		generator: getTopByProperty<CoreStatsResult>('new_bird_count')
+	},
+	{
+		type: 'juvs',
+		category: 'count',
+		unit: 'bird',
+		verb: 'Most juveniles in a',
+		generator: getTopByPropertiesSum<CoreStatsResult>([
+			'pullus_bird_count',
+			'juv_bird_count',
+			'postjuv_bird_count'
+		])
+	}
+];
--- app/lib/highlights/v2/index.ts
+++ app/lib/highlights/v2/index.ts
@@ -3,7 +3,7 @@ import {
 	type RawStats
 } from '@/app/actions/highlights-data';
 import type { CoreStatsResult } from '@/app/models/db';
-const DEFAULT_OPTIONS = { limit: 3, threshold: 0 };
+import { DEFAULT_OPTIONS, highlightRules } from './highlight-rules';
 export type OneBasedMonth = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export type YearMonthRestriction = {
@@ -18,14 +18,6 @@ type HighlightType = 'birds' | 'encounters' | 'species' | 'newBirds' | 'juvs';
 export type Highlight = {
 	time_period: string;
 	value: number;
-};
-
-interface TimePeriodedItem {
-	time_period: string | null;
-}
-
-type HighlightFinderOptions = {
-	threshold?: number;
 };
 
 export type HighlightsOfType = {
@@ -51,39 +43,6 @@ export type HighlightInContext = {
 	position: number;
 	isTied: boolean;
 };
-
-function sumProperties<T>(item: T, properties: (keyof T)[]) {
-	return properties.reduce(
-		(sum, property) => sum + ((item[property] as number) ?? 0),
-		0
-	);
-}
-
-function getTopByPropertiesSum<T extends TimePeriodedItem>(
-	properties: (keyof T)[],
-	rawStats: T[],
-	options?: HighlightFinderOptions
-): Highlight[] {
-	const { threshold } = {
-		...DEFAULT_OPTIONS,
-		...(options || {})
-	};
-	return rawStats
-		.map((item) => ({
-			time_period: item.time_period as string,
-			value: sumProperties(item, properties)
-		}))
-		.filter((item) => item.value > threshold)
-		.sort((a, b) => b.value - a.value);
-}
-
-function getTopByProperty<T extends TimePeriodedItem>(
-	property: keyof T,
-	rawStats: T[],
-	options?: HighlightFinderOptions
-): Highlight[] {
-	return getTopByPropertiesSum([property], rawStats, options);
-}
 
 function applyLimitToHighlight(
 	highlightWrapper: HighlightsOfType,
@@ -115,6 +74,10 @@ function applyLimitToHighlights(highlights: HighlightsOfType[], limit: number) {
 
 const cache: Map<string, HighlightsOfType[]> = new Map();
 
+function isHighlightsOfType(item: unknown): item is HighlightsOfType {
+	return Boolean(item);
+}
+
 function generateHighlightsFromStats({
 	stats,
 	limit,
@@ -130,71 +93,18 @@ function generateHighlightsFromStats({
 	if (cache.has(cacheKey)) {
 		unboundedHighlights = cache.get(cacheKey) as HighlightsOfType[];
 	} else {
-		unboundedHighlights = [
-			{
-				type: 'birds',
-				unit: 'bird',
-				verb: 'Busiest',
-				temporalUnit,
-				category: 'count',
-				highlights: getTopByProperty<CoreStatsResult>(
-					'bird_count',
-					stats.overall
-				)
-			},
-			...(temporalUnit === 'day'
-				? []
-				: [
-						{
-							type: 'encounters',
-							unit: 'encounter',
-							verb: 'Most encounters per',
-							category: 'count',
-							temporalUnit,
-							highlights: getTopByProperty<CoreStatsResult>(
-								'encounter_count',
-								stats.overall
-							)
-						} as HighlightsOfType
-					]),
-			{
-				type: 'species',
-				unit: 'species',
-				verb: 'Most varied',
-				temporalUnit,
-				category: 'count',
-				highlights: getTopByProperty<CoreStatsResult>(
-					'species_count',
-					stats.overall
-				)
-			},
-			{
-				type: 'newBirds',
-				category: 'count',
-				unit: 'bird',
-				verb: 'Most new birds in a',
-				temporalUnit,
-				highlights: getTopByProperty<CoreStatsResult>(
-					'new_bird_count',
-					stats.overall
-				)
-			},
-			{
-				type: 'juvs',
-				category: 'count',
-				unit: 'bird',
-				verb: 'Most juveniles in a',
-				temporalUnit,
-				highlights: getTopByPropertiesSum<CoreStatsResult>(
-					['pullus_bird_count', 'juv_bird_count', 'postjuv_bird_count'],
-					stats.overall
-				)
-			}
-			// bySpecies: {
-			//   // todo - upstream turn bySpecies into a better data structure to work with
-			//   counts: counts(dailyStats.bySpecies)
-			// }
-		];
+		unboundedHighlights = highlightRules
+			.map((rule) => {
+				if (rule.condition && !rule.condition(temporalUnit)) return null;
+				const highlights: HighlightsOfType = {
+					...rule,
+					temporalUnit,
+					highlights: rule.generator(stats.overall)
+				};
+				return highlights;
+			})
+			.filter(isHighlightsOfType);
+
 		cache.set(cacheKey, unboundedHighlights);
 	}
 	return applyLimitToHighlights(
